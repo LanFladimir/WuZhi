f617e52515be81f50fcb20ea3000836c087af193
LanFladimir <LanFladimir@outlook.com>
2018-01-18 09:48:15 +0800

    kotlin

---
+++ Wuzhi_k/src/main/java/wuzhi/fladimir/com/wuzhi_k/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package wuzhi.fladimir.com.wuzhi_k
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
